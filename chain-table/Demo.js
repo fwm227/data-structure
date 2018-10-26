6bf9670267fb438c41ec3cb6403ba6f0ea32ac8b
Mirok <927627286@qq.com>
2018-10-26 09:31:42 +0800

    add indexOf, remove, isEmpty, size and toString function

--- chain-table/Demo.js
+++ chain-table/Demo.js
@@ -22,10 +22,12 @@ function linkList () {
   this.insert = function (element, position) {
     let node = new Node(element), previous, index = 0, current = head;
     if (position === 0) {
+      // insert head
       node.next = current;
       head = node;
     } else {
-      // move next node
+      // insert center
+      // move to next node
       while (index++ < position) {
         previous = current;
         current = current.next;
@@ -38,6 +40,7 @@ function linkList () {
   this.removeAt = function (position) {
     let current = head, removeNode, previous, index = 0;
     if (position === 0) {
+      // remove head node
       head = current.next;
     } else {
       while (index++ < position) {
@@ -48,18 +51,59 @@ function linkList () {
       length--;
     }
   }
+  // get index-value of element
+  this.indexOf = function (element) {
+    let index = 0, current = head;
+    while (current) {
+      if (current.val === element) {
+        return index;
+      }
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
   // remove node according to node-value
   this.remove = function (element) {
-
+    this.removeAt(this.indexOf(element));
   }
-  // show chian-table
-  this.show = function () {
-    console.log(JSON.stringify(head));
+  // judge the chain-table whether is null
+  this.isEmpty = function () {
+    if (head === null) {
+      return true;
+    }
+    return false;
+  }
+  // get chain-table size
+  this.size = function () {
+    return length;
+  }
+  // transform chain-table to string
+  this.toString = function () {
+    let current = head, stringVal = '';
+    while (current) {
+      stringVal += current.val;
+      current = current.next;
+    }
+    return stringVal;
+  }
+  // show chain-table
+  this.getHead = function () {
+    return head;
+  }
+  // print chain-table
+  this.print = function () {
+    console.log(head);
   }
 }
 const chainList = new linkList();
 chainList.append(1);
 chainList.append(2);
 chainList.insert(8, 1);
-chainList.removeAt(1);
-chainList.show()
+chainList.insert(7, 1);
+chainList.getHead();
+console.log(chainList.indexOf(8));
+chainList.remove(2);
+chainList.print();
+console.log(chainList.isEmpty());
+console.log(chainList.toString());

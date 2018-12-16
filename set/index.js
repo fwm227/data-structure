4c3ea95e00f6c604ca26ccefeda61ab4c34d44f1
clm960227 <927627286@qq.com>
2018-12-16 11:48:42 +0800

    add union, intersection, difference and isSubset funtion in set data-structure

--- set/index.js
+++ set/index.js
@@ -1,47 +1,89 @@
-/*add(value) ：向集合添加一个新的项。
-delete(value) ：从集合移除一个值。
-has(value) ：如果值在集合中，返回 true ，否则返回 false 。
-clear() ：移除集合中的所有项。
-size() ：返回集合所包含元素的数量。与数组的 length 属性类似。
-values() ：返回一个包含集合中所有值的数组。*/
-
+/*add(value)： 向集合添加一个新的项。
+delete(value)： 从集合移除一个值。
+has(value)： 如果值在集合中，返回 true ，否则返回 false 。
+clear()： 移除集合中的所有项。
+size()： 返回集合所包含元素的数量。与数组的 length 属性类似。
+values()： 返回一个包含集合中所有值的数组。
+nuion: 并集
+intersection: 交集
+complement: 补集
+isSubset: 判断是否是子集*/
 function Set () {
-  let setList = {};
+  this.setList = {};
   // add into set
   this.add = function (item) {
     if (!this.has(item)) {
-      setList[item] = item;
+      this.setList[item] = item;
     }
   }
   // delete set item
   this.delete = function (item) {
     if (this.has(item)) {
-      delete setList[item];
+      delete this.setList[item];
     }
   }
   // judge set has value
   this.has = function (item) {
-    return setList.hasOwnProperty(item)
+    return this.setList.hasOwnProperty(item)
   }
   // clear set
   this.clear = function () {
-    setList = {};
+    this.setList = {};
   }
   // get set size
   this.size = function () {
-    return Object.keys(setList).length;
+    return Object.keys(this.setList).length;
   }
   // get set values
   this.values = function () {
-    let values = [], keyList = Object.keys(setList);
+    let values = [], keyList = Object.keys(this.setList);
     for (let i = 0; i < keyList.length; i++) {
-      values.push(setList[keyList[i]]);
+      values.push(this.setList[keyList[i]]);
     }
     return values;
   }
+  this.union = function (set) {
+    let keys = Object.keys(set.setList);
+    for (let i = 0; i < keys.length; i++) {
+      this.add(set.setList[keys[i]]);
+    }
+  }
+  this.intersection = function (set) {
+    let interSet = new Set();
+    let keys = Object.keys(set.setList);
+    for (let i = 0; i < keys.length; i++) {
+      if (this.has(set.setList[keys[i]])) {
+        interSet.add(set.setList[keys[i]]);
+      }
+    }
+    return interSet.setList;
+  }
+  this.differenct = function (set) {
+    let compSet = new Set();
+    let keys = Object.keys(this.setList);
+    for (let i = 0; i < keys.length; i++) {
+      if (!set.has(this.setList[keys[i]])) {
+        compSet.add(this.setList[keys[i]])
+      }
+    }
+    return compSet.setList;
+  }
+  this.isSubset = function (set) {
+    if (this.size() < set.size()) {
+      return false;
+    } else {
+      let keys = Object.keys(set.setList);
+      for (let i = 0; i < keys.length; i++) {
+        if (!this.has(set.setList[keys[i]])) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
   // print set
   this.print = function () {
-    console.log(setList);
+    console.log(this.setList);
   }
 }
 
@@ -51,11 +93,20 @@ set.add(2);
 set.add(3);
 set.add(2);
 set.delete(1);
-set.print();
+// set.print();
 // get set size
-console.log(set.size());
+// console.log(set.size());
 // get values
-console.log(set.values());
+// console.log(set.values());
+const set2 = new Set();
+set2.add(2);
+set2.add(7);
+set2.add(8);
+// set.union(set2);
+// console.log(set.intersection(set2));
+// console.log(set.differenct(set2));
+console.log(set.isSubset(set2));
+// set.print();
 // clear set
-set.clear();
-set.print();
+// set.clear();
+// set.print();

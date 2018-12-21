c4c6c3a8da7ef9c7b870f2238cf9ee17a1922df1
Mirok <927627286@qq.com>
2018-12-21 10:43:34 +0800

    add remove node in BST

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -119,12 +119,48 @@ function BinarySearchTree () {
     }
     return null;
   }
+  // remove node from BST
+  this.remove = function (key) {
+    removeNode(root, key);
+  }
+  function removeNode (node, key) {
+    if (node === null) {
+      return null;
+    }
+    if (node.key > key) {
+      node.left = removeNode(node.left, key);
+      return node;
+    } else if (node.key < key) {
+      node.right = removeNode(node.right, key);
+      return node;
+    } else {
+      // node.key equal width search key
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      if (node.left === null) {
+        node = node.right; 
+        return node;
+      }
+      if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      // two children-node situation
+      var mNode = findMIN(node.right);
+      node.key = mNode;
+      node.right = removeNode(node.right, mNode);
+      return node;
+    }
+  }
 }
 
 var binarySearchTree = new BinarySearchTree();
 binarySearchTree.insert(11);
 binarySearchTree.insert(2);
 binarySearchTree.insert(3);
+binarySearchTree.insert(4);
 binarySearchTree.insert(12);
 binarySearchTree.insert(13);
 // binarySearchTree.print(1);
@@ -133,5 +169,7 @@ binarySearchTree.insert(13);
 // binarySearchTree.print();
 // search node
 // console.log(binarySearchTree.search(3));
-binarySearchTree.find(0);
-binarySearchTree.find(1);
+// binarySearchTree.find(0);
+// binarySearchTree.find(1);
+// binarySearchTree.remove(2);
+binarySearchTree.print(1);

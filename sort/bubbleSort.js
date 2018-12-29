1f3dd32a25b43213d25cd2055135990f268448f4
Mirok <927627286@qq.com>
2018-12-29 15:02:34 +0800

    fix bubble sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -9,14 +9,14 @@ Array.prototype.swap = function (idx1, idx2) {
 }
 function bubbleSort (arr, type) {
   for (var i = 0; i < arr.length-1; i++) {
-    for (var j = i + 1; j < arr.length; j++) {
+    for (var j = 0; j < arr.length-1-i; j++) {
       // type 0 indicates that is from smallest to lagest,1 indicates reverse mean
       if (type === 0) {
-        if (arr[i] > arr[j]) {
+        if (arr[j] > arr[j + 1]) {
           Array.prototype.swap.call(arr, i, j);
         }
       } else if (type === 1) {
-        if (arr[i] < arr[j]) {
+        if (arr[j] < arr[j + 1]) {
           Array.prototype.swap.call(arr, i, j);
         }
       }
